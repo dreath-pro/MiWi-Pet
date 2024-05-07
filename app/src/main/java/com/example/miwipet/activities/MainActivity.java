703de8806dc7f56f37859dee54d199a02a0bf063
dreath <75094352+dreath-pro@users.noreply.github.com>
2024-05-07 16:26:14 +0800

    egg database adjustment

--- app/src/main/java/com/example/miwipet/activities/MainActivity.java
+++ app/src/main/java/com/example/miwipet/activities/MainActivity.java
@@ -180,6 +180,7 @@ public class MainActivity extends AppCompatActivity {
 
                     petDatabase.addPet(petModel);
                     getPetFromDatabase();
+                    getEggFromDatabase();
 
                     incubated.clear();
                 } else {
