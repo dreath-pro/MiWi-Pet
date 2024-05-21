b7d7504a8f9163d31705d25866689a67b3a8c2ef
dreath <75094352+dreath-pro@users.noreply.github.com>
2024-05-21 16:12:16 +0800

    savanna pets art fixed

--- app/src/main/java/com/example/miwipet/models/eggs/SavannaEgg.java
+++ app/src/main/java/com/example/miwipet/models/eggs/SavannaEgg.java
@@ -10,30 +10,30 @@ public class SavannaEgg extends EggModel {
 
 
             {R.drawable.zebra, R.drawable.cheetah, R.drawable.gazelle, R.drawable.vine_snake,
-                    R.drawable.mandrill},
+                    R.drawable.mandrill, R.drawable.african_buffalo},
 
 
             {R.drawable.ostrich, R.drawable.vulture, R.drawable.wildebeest, R.drawable.african_wild_dog,
-                    R.drawable.caracal},
+                    R.drawable.caracal, R.drawable.impala},
 
 
             {R.drawable.giraffe, R.drawable.hyena, R.drawable.lion, R.drawable.leopard,
                     R.drawable.leopard_turtle, R.drawable.lynx},
 
 
-            {R.drawable.sun_war_lion, R.drawable.death_vulture}
+            {R.drawable.sun_war_lion, R.drawable.death_vulture, R.drawable.viper_snake}
     };
 
     String[][] petNames = {
             {"Elephant", "Rhino", "Warthog", "Lemur", "Black Mamba"},
 
-            {"Zebra", "Cheetah", "Gazelle", "Vine Snake", "Mandrill"},
+            {"Zebra", "Cheetah", "Gazelle", "Vine Snake", "Mandrill", "African Buffalo"},
 
-            {"Ostrich", "Vulture", "Wildebeest", "African Wild Dog", "Caracal"},
+            {"Ostrich", "Vulture", "Wildebeest", "African Wild Dog", "Caracal", "Impala"},
 
             {"Giraffe", "Hyena", "Lion", "Leopard", "Leopard Turtle", "Lynx"},
 
-            {"Sun War Lion", "Death Vulture"}
+            {"Sun War Lion", "Death Vulture", "Viper Snake"}
     };
 
     public SavannaEgg() {
