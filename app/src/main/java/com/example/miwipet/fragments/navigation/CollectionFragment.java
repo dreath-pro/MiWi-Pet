ec0a1c349339356a930c052d3064ee15cbfbfa5e
dreath <75094352+dreath-pro@users.noreply.github.com>
2024-06-07 14:40:21 +0800

    inventory sorting

--- app/src/main/java/com/example/miwipet/fragments/navigation/CollectionFragment.java
+++ app/src/main/java/com/example/miwipet/fragments/navigation/CollectionFragment.java
@@ -27,6 +27,7 @@ import com.example.miwipet.models.InventoryModel;
 import com.example.miwipet.models.ObjectModel;
 import com.example.miwipet.models.PetModel;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -38,15 +39,23 @@ public class CollectionFragment extends Fragment {
     private ImageView backPageButton, nextPageButton;
     private TextView pageIndicator;
 
-    private int page = 1;
-    private int selectedItem = 0;
+    private int page;
+    private int maxPage;
+    private int maxSize;
+    private int selectedItem;
+    private boolean lockSort;
 
     private InventoryModel inventoryModel;
 
-    public CollectionFragment(InventoryModel inventoryModel, Context context)
-    {
+    public CollectionFragment(InventoryModel inventoryModel, Context context) {
         this.inventoryModel = inventoryModel;
         this.context = context;
+
+        lockSort = false;
+        page = 1;
+        maxPage = 1;
+        maxSize = 50;
+        selectedItem = 0;
     }
 
     @Override
@@ -69,6 +78,8 @@ public class CollectionFragment extends Fragment {
         petsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                inventorySearch.setText("");
+                page = 1;
                 showPetCollection(false, "");
             }
         });
@@ -76,6 +87,8 @@ public class CollectionFragment extends Fragment {
         objectsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                inventorySearch.setText("");
+                page = 1;
                 showObjectCollection(false, "");
             }
         });
@@ -83,6 +96,8 @@ public class CollectionFragment extends Fragment {
         foodsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                inventorySearch.setText("");
+                page = 1;
                 showFoodCollection(false, "");
             }
         });
@@ -94,10 +109,36 @@ public class CollectionFragment extends Fragment {
                 if (page < 1) {
                     page = 1;
                 } else {
-                    //resetAdapter();
+                    switch (selectedItem) {
+                        case 0:
+                            if(inventorySearch.getText().toString().isEmpty())
+                            {
+                                showPetCollection(false, "");
+                            }else
+                            {
+                                showPetCollection(true, inventorySearch.getText().toString());
+                            }
+                            break;
+                        case 1:
+                            if(inventorySearch.getText().toString().isEmpty())
+                            {
+                                showFoodCollection(false, "");
+                            }else
+                            {
+                                showFoodCollection(true, inventorySearch.getText().toString());
+                            }
+                            break;
+                        case 2:
+                            if(inventorySearch.getText().toString().isEmpty())
+                            {
+                                showObjectCollection(false, "");
+                            }else
+                            {
+                                showObjectCollection(true, inventorySearch.getText().toString());
+                            }
+                            break;
+                    }
                 }
-
-                pageIndicator.setText(page + " out of 10");
             }
         });
 
@@ -105,13 +146,39 @@ public class CollectionFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 page++;
-                if (page > 10) {
-                    page = 10;
+                if (page > maxPage) {
+                    page = maxPage;
                 } else {
-                    //resetAdapter();
+                    switch (selectedItem) {
+                        case 0:
+                            if(inventorySearch.getText().toString().isEmpty())
+                            {
+                                showPetCollection(false, "");
+                            }else
+                            {
+                                showPetCollection(true, inventorySearch.getText().toString());
+                            }
+                            break;
+                        case 1:
+                            if(inventorySearch.getText().toString().isEmpty())
+                            {
+                                showFoodCollection(false, "");
+                            }else
+                            {
+                                showFoodCollection(true, inventorySearch.getText().toString());
+                            }
+                            break;
+                        case 2:
+                            if(inventorySearch.getText().toString().isEmpty())
+                            {
+                                showObjectCollection(false, "");
+                            }else
+                            {
+                                showObjectCollection(true, inventorySearch.getText().toString());
+                            }
+                            break;
+                    }
                 }
-
-                pageIndicator.setText(page + " out of 10");
             }
         });
 
@@ -132,21 +199,25 @@ public class CollectionFragment extends Fragment {
             }
         });
 
+        inventorySearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inventorySearch.setText("");
+            }
+        });
+
         return view;
     }
 
-    private void filterInventory(String query)
-    {
+    private void filterInventory(String query) {
         /**
          * items
          * 0 - pets
          * 1 - foods
          * 2 - objects
          */
-        if(!query.isEmpty())
-        {
-            switch (selectedItem)
-            {
+        if (!query.isEmpty()) {
+            switch (selectedItem) {
                 case 0:
                     showPetCollection(true, query);
                     break;
@@ -157,10 +228,8 @@ public class CollectionFragment extends Fragment {
                     showObjectCollection(true, query);
                     break;
             }
-        }else
-        {
-            switch (selectedItem)
-            {
+        } else {
+            switch (selectedItem) {
                 case 0:
                     showPetCollection(false, "");
                     break;
@@ -174,78 +243,197 @@ public class CollectionFragment extends Fragment {
         }
     }
 
-    private void showPetCollection(boolean filtered, String query)
-    {
+    private InventoryModel sortInventory(InventoryModel inventoryModel) {
+        InventoryModel sortedInventory = new InventoryModel();
+
+        for (int i = inventoryModel.getPetLists().size() - 1; i >= 0; i--) {
+            sortedInventory.getPetLists().add(inventoryModel.getPetLists().get(i));
+        }
+
+        for (int i = inventoryModel.getFoodLists().size() - 1; i >= 0; i--) {
+            sortedInventory.getFoodLists().add(inventoryModel.getFoodLists().get(i));
+        }
+
+        for (int i = inventoryModel.getObjectLists().size() - 1; i >= 0; i--) {
+            sortedInventory.getObjectLists().add(inventoryModel.getObjectLists().get(i));
+        }
+
+        lockSort = true;
+
+        return sortedInventory;
+    }
+
+    private void showPetCollection(boolean filtered, String query) {
         selectedItem = 0;
         ArrayList<PetModel> petLists = new ArrayList<>();
 
-        if(!filtered)
-        {
+        if (!lockSort) {
+            inventoryModel = sortInventory(inventoryModel);
+        }
+
+        if (!filtered) {
             petLists = inventoryModel.getPetLists();
-        }else
-        {
-            for(PetModel pet : inventoryModel.getPetLists())
-            {
-                if(pet.getPetName().toLowerCase().contains(query.toLowerCase()))
-                {
+        } else {
+            for (PetModel pet : inventoryModel.getPetLists()) {
+                if (pet.getPetName().toLowerCase().contains(query.toLowerCase())) {
                     petLists.add(pet);
                 }
             }
         }
 
+
+        int minIndex = 0, maxIndex = maxSize - 1;
+        if (petLists.size() < maxSize) {
+            maxIndex = petLists.size() - 1;
+        }
+
+        maxPage = 1;
+        int tempMaxSize = maxSize;
+        for (int i = 1; i <= petLists.size(); i++) {
+            if (i > tempMaxSize) {
+                maxPage++;
+                tempMaxSize += maxSize;
+            }
+        }
+
+        for (int i = 1; i <= page - 1; i++) {
+            minIndex = maxIndex + 1;
+            maxIndex += maxSize;
+        }
+
+        ArrayList<PetModel> tempList = new ArrayList<>();
+        for (int i = minIndex; i <= maxIndex; i++) {
+            if(i > petLists.size() - 1)
+            {
+                break;
+            }
+            tempList.add(petLists.get(i));
+        }
+
+        petLists = tempList;
+
+
         GridLayoutManager petLayoutManager = new GridLayoutManager(context, 2);
         collectionView.setLayoutManager(petLayoutManager);
         PetAdapter petAdapter = new PetAdapter(getActivity(), petLists);
         collectionView.setAdapter(petAdapter);
+
+        pageIndicator.setText(page + " out of " + maxPage);
     }
 
-    private void showFoodCollection(boolean filtered, String query)
-    {
+    private void showFoodCollection(boolean filtered, String query) {
         selectedItem = 1;
         ArrayList<FoodModel> foodLists = new ArrayList<>();
 
-        if(!filtered)
-        {
+        if (!lockSort) {
+            inventoryModel = sortInventory(inventoryModel);
+        }
+
+        if (!filtered) {
             foodLists = inventoryModel.getFoodLists();
-        }else
-        {
-            for(FoodModel food : inventoryModel.getFoodLists())
-            {
-                if(food.getFoodName().toLowerCase().contains(query.toLowerCase()))
-                {
+        } else {
+            for (FoodModel food : inventoryModel.getFoodLists()) {
+                if (food.getFoodName().toLowerCase().contains(query.toLowerCase())) {
                     foodLists.add(food);
                 }
             }
         }
 
+
+        int minIndex = 0, maxIndex = maxSize - 1;
+        if (foodLists.size() < maxSize) {
+            maxIndex = foodLists.size() - 1;
+        }
+
+        maxPage = 1;
+        int tempMaxSize = maxSize;
+        for (int i = 1; i <= foodLists.size(); i++) {
+            if (i > tempMaxSize) {
+                maxPage++;
+                tempMaxSize += maxSize;
+            }
+        }
+
+        for (int i = 1; i <= page - 1; i++) {
+            minIndex = maxIndex + 1;
+            maxIndex += maxSize;
+        }
+
+        ArrayList<FoodModel> tempList = new ArrayList<>();
+        for (int i = minIndex; i <= maxIndex; i++) {
+            if(i > foodLists.size() - 1)
+            {
+                break;
+            }
+            tempList.add(foodLists.get(i));
+        }
+
+        foodLists = tempList;
+
+
         GridLayoutManager foodLayoutManager = new GridLayoutManager(context, 2);
         collectionView.setLayoutManager(foodLayoutManager);
         FoodAdapter foodAdapter = new FoodAdapter(getActivity(), foodLists);
         collectionView.setAdapter(foodAdapter);
+
+        pageIndicator.setText(page + " out of " + maxPage);
     }
 
-    private void showObjectCollection(boolean filtered, String query)
-    {
+    private void showObjectCollection(boolean filtered, String query) {
         selectedItem = 2;
         ArrayList<ObjectModel> objectLists = new ArrayList<>();
 
-        if(!filtered)
-        {
+        if (!lockSort) {
+            inventoryModel = sortInventory(inventoryModel);
+        }
+
+        if (!filtered) {
             objectLists = inventoryModel.getObjectLists();
-        }else
-        {
-            for(ObjectModel object : inventoryModel.getObjectLists())
-            {
-                if(object.getObjectName().toLowerCase().contains(query.toLowerCase()))
-                {
+        } else {
+            for (ObjectModel object : inventoryModel.getObjectLists()) {
+                if (object.getObjectName().toLowerCase().contains(query.toLowerCase())) {
                     objectLists.add(object);
                 }
             }
         }
 
+
+        int minIndex = 0, maxIndex = maxSize - 1;
+        if (objectLists.size() < maxSize) {
+            maxIndex = objectLists.size() - 1;
+        }
+
+        maxPage = 1;
+        int tempMaxSize = maxSize;
+        for (int i = 1; i <= objectLists.size(); i++) {
+            if (i > tempMaxSize) {
+                maxPage++;
+                tempMaxSize += maxSize;
+            }
+        }
+
+        for (int i = 1; i <= page - 1; i++) {
+            minIndex = maxIndex + 1;
+            maxIndex += maxSize;
+        }
+
+        ArrayList<ObjectModel> tempList = new ArrayList<>();
+        for (int i = minIndex; i <= maxIndex; i++) {
+            if(i > objectLists.size() - 1)
+            {
+                break;
+            }
+            tempList.add(objectLists.get(i));
+        }
+
+        objectLists = tempList;
+
+
         GridLayoutManager objectLayoutManager = new GridLayoutManager(context, 2);
         collectionView.setLayoutManager(objectLayoutManager);
         ObjectAdapter objectAdapter = new ObjectAdapter(getActivity(), objectLists);
         collectionView.setAdapter(objectAdapter);
+
+        pageIndicator.setText(page + " out of " + maxPage);
     }
 }
